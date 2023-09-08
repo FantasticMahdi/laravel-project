09e63bbe03c5460d0af74c5c8ab910bf5f03845c
FantasticMahdi <mahdi.khrasani95@gmail.com>
2023-09-08 18:15:02 +0330

    part-21 (sidebar dropdown hide/show)

--- assets/js/grid.js
+++ assets/js/grid.js
@@ -1,7 +1,6 @@
-$(document).ready(function() {
+$(document).ready(function () {
 
-    function removeAllSidebarToggleClass()
-    {
+    function removeAllSidebarToggleClass() {
         $('#sidebar-toggle-hide').removeClass('d-md-inline');
         $('#sidebar-toggle-hide').removeClass('d-none');
         $('#sidebar-toggle-show').removeClass('d-inline');
@@ -9,10 +8,12 @@ $(document).ready(function() {
     }
 
 
-    $('#sidebar-toggle-hide').click(function() {
+    $('#sidebar-toggle-hide').click(function () {
         $('#sidebar').fadeOut(300);
-        $('#main-body').animate({width : "100%"},300);
-        setTimeout(function() {
+        $('#main-body').animate({
+            width: "100%"
+        }, 300);
+        setTimeout(function () {
             removeAllSidebarToggleClass();
             $('#sidebar-toggle-hide').addClass('d-none');
             $('#sidebar-toggle-show').removeClass('d-none');
@@ -21,9 +22,9 @@ $(document).ready(function() {
 
 
 
-    $('#sidebar-toggle-show').click(function() {
+    $('#sidebar-toggle-show').click(function () {
         $('#sidebar').fadeIn(300);
-        setTimeout(function() {
+        setTimeout(function () {
             removeAllSidebarToggleClass();
             $('#sidebar-toggle-hide').removeClass('d-none');
             $('#sidebar-toggle-show').addClass('d-none');
@@ -31,37 +32,63 @@ $(document).ready(function() {
     });
 
 
-    $('#menu-toggle').click(function() {
+    $('#menu-toggle').click(function () {
         $('#body-header').toggle(300);
     });
-    
-    $('#search-toggle').click(function() {
+
+    $('#search-toggle').click(function () {
         $('#search-toggle').removeClass('d-md-inline');
         $('#search-area').addClass('d-md-inline');
-        $('#search-input').animate({width : "12rem"},300);
+        $('#search-input').animate({
+            width: "12rem"
+        }, 300);
     });
-    
-    
-    $('#search-area-hide').click(function() {
-        $('#search-input').animate({width : "0"},300);
-        setTimeout(function(){
+
+
+    $('#search-area-hide').click(function () {
+        $('#search-input').animate({
+            width: "0"
+        }, 300);
+        setTimeout(function () {
             $('#search-toggle').addClass('d-md-inline');
             $('#search-area').removeClass('d-md-inline');
         }, 300);
     });
 
 
-    $('#header-notification-toggle').click(function() {
+    $('#header-notification-toggle').click(function () {
         $('#header-notification').fadeToggle();
     });
 
 
-    $('#header-comment-toggle').click(function() {
+    $('#header-comment-toggle').click(function () {
         $('#header-comment').fadeToggle();
     });
 
 
-    $('#header-profile-toggle').click(function() {
+    $('#header-profile-toggle').click(function () {
         $('#header-profile').fadeToggle();
     });
+
+
+    $('.sidebar-group-link').click(function () {
+        $('.sidebar-group-link').removeClass('sidebar-group-link-active');
+        $('.sidebar-group-link').children('.sidebar-dropdown-toggle').children('.angle').removeClass('fa-angle-down');
+        $('.sidebar-group-link').children('.sidebar-dropdown-toggle').children('.angle').addClass('fa-angle-left');
+        
+        
+        $(this).addClass('sidebar-group-link-active');
+        
+        $(this).children('.sidebar-dropdown-toggle').children('.angle').removeClass('fa-angle-left');
+        
+        $(this).children('.sidebar-dropdown-toggle').children('.angle').addClass('fa-angle-down');
+        
+    });
+    // $('.sidebar-group-link-active').click(function () {
+    // $('.sidebar-group-link-active').removeClass('sidebar-group-link');
+
+    // $('.sidebar-group-link').children('.sidebar-dropdown-toggle').children('.angle').removeClass('fa-angle-down');
+
+    // $('.sidebar-group-link').children('.sidebar-dropdown-toggle').children('.angle').addClass('fa-angle-left');
+    // });
 })
