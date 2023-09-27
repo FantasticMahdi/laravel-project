bcc5e635f885fdc7436c8cc329ed620c5517bcf8
FantasticMahdi <mahdi.khrasani95@gmail.com>
2023-09-27 12:30:02 +0330

    part-67 (postCategory store)

--- app/Http/Controllers/Admin/Content/CategoryController.php
+++ app/Http/Controllers/Admin/Content/CategoryController.php
@@ -37,7 +37,7 @@ class CategoryController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        
     }
 
     /**
--- resources/views/admin/content/category/create.blade.php
+++ resources/views/admin/content/category/create.blade.php
@@ -27,22 +27,43 @@
                 </section>
 
                 <section>
-                    <form action="" method="post">
+                    <form action="{{ route('admin.content.category.store') }}" method="post" enctype="multipart/form-data">
+                        @csrf
                         <section class="row">
                             <section class="col-12 col-md-6">
                                 <div class="form-group">
-                                    <label for="">نام دسته</label>
-                                    <input class="form-control form-control-sm" type="text" name=""
-                                        id="">
+                                    <label for="name">نام دسته</label>
+                                    <input class="form-control form-control-sm" type="text" name="name"
+                                        id="name">
                                 </div>
                             </section>
                             <section class="col-12 col-md-6">
                                 <div class="form-group">
-                                    <label for="">نام والد</label>
-                                    <select class="form-control form-control-sm" name="" id="">
-                                        <option value="">choose category</option>
-                                        <option value="">electronic</option>
+                                    <label for="tags">تگ ها</label>
+                                    <input class="form-control form-control-sm" type="text" name="tags"
+                                        id="tags">
+                                </div>
+                            </section>
+                            <section class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="image">تصویر</label>
+                                    <input class="form-control form-control-sm" type="file" name="image"
+                                        id="image">
+                                </div>
+                            </section>
+                            <section class="col-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="status">وضعیت</label>
+                                    <select class="form-control form-control-sm" name="status" id="status">
+                                        <option value="0">غیر فعال</option>
+                                        <option value="1">فعال</option>
                                     </select>
+                                </div>
+                            </section>
+                            <section class="col-12">
+                                <div class="form-group">
+                                    <label for="">توضیحات</label>
+                                    <textarea class="form-control form-control-sm" name="description" id="description" rows="4"></textarea>
                                 </div>
                             </section>
                             <section class="col-12">
@@ -54,4 +75,11 @@
             </section>
         </section>
     </section>
+@endsection
+
+@section('script')
+    <script src="{{ asset('admin-assets/ckeditor/ckeditor.js') }}"></script>
+    <script>
+        CKEDITOR.replace('description');
+    </script>
 @endsection
