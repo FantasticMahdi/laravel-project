44a3184cfbf2109a51e733a151caecb1f03eb7c2
FantasticMahdi <mahdi.khrasani95@gmail.com>
2023-09-28 22:58:57 +0330

    part-72 (use select2 service)

--- resources/views/admin/content/category/create.blade.php
+++ resources/views/admin/content/category/create.blade.php
@@ -27,58 +27,71 @@
                 </section>
 
                 <section>
-                    <form action="{{ route('admin.content.category.store') }}" method="post" enctype="multipart/form-data">
+                    <form action="{{ route('admin.content.category.store') }}" method="post" enctype="multipart/form-data" id="form">
                         @csrf
                         <section class="row">
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="name">نام دسته</label>
-<input class="form-control form-control-sm" type="text" name="name" id="name" value="{{ old('name') }}">
+                                    <input class="form-control form-control-sm" type="text" name="name" id="name"
+                                        value="{{ old('name') }}">
 
                                 </div>
                                 @error('name')
-<span class="alert_required bg-danger text-white p-1 rounded" role="alert"><strong>{{ $message }}</strong></span>
-                                    @enderror
+                                    <span class="alert_required bg-danger text-white p-1 rounded"
+                                        role="alert"><strong>{{ $message }}</strong></span>
+                                @enderror
                             </section>
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
-                                    <label for="tags">تگ ها</label>
-<input class="form-control form-control-sm" type="text" name="tags" id="tags" value="{{ old('tags') }}">
+<label for="tags">تگ ها</label>
+<input class="form-control form-control-sm" type="hidden" name="tags" id="tags"
+value="{{ old('tags') }}">
+<select name="" class="select3 form-control form-control-sm" id="select_tags" multiple>
+
+                                    </select>
                                 </div>
                                 @error('tags')
-<span class="alert_required bg-danger text-white p-1 rounded" role="alert"><strong>{{ $message }}</strong></span>
-                                    @enderror
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </section>
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="image">تصویر</label>
                                     <input class="form-control form-control-sm" type="file" name="image"
-id="image">
+                                        id="image">
                                 </div>
                                 @error('image')
-<span class="alert_required bg-danger text-white p-1 rounded" role="alert"><strong>{{ $message }}</strong></span>
-                                    @enderror
+                                    <span class="alert_required bg-danger text-white p-1 rounded"
+                                        role="alert"><strong>{{ $message }}</strong></span>
+                                @enderror
                             </section>
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
                                     <label for="status">وضعیت</label>
                                     <select class="form-control form-control-sm" name="status" id="status">
-<option value="0" @if (old('status') == 0) selected @endif>غیر فعال</option>
-<option value="1" @if (old('status') == 1) selected @endif>فعال</option>
+                                        <option value="0" @if (old('status') == 0) selected @endif>غیر فعال
+                                        </option>
+                                        <option value="1" @if (old('status') == 1) selected @endif>فعال
+                                        </option>
                                     </select>
                                 </div>
                                 @error('status')
-<span class="alert_required bg-danger text-white p-1 rounded" role="alert"><strong>{{ $message }}</strong></span>
-                                    @enderror
+                                    <span class="alert_required bg-danger text-white p-1 rounded"
+                                        role="alert"><strong>{{ $message }}</strong></span>
+                                @enderror
                             </section>
                             <section class="col-12">
                                 <div class="form-group">
                                     <label for="">توضیحات</label>
-<textarea class="form-control form-control-sm" name="description" id="description" rows="4">{{ old('description') }}</textarea>
+                                    <textarea class="form-control form-control-sm" name="description" id="description" rows="4">{{ old('description') }}</textarea>
                                 </div>
                                 @error('description')
-<span class="alert_required bg-danger text-white p-1 rounded" role="alert"><strong>{{ $message }}</strong></span>
-                                    @enderror
+                                    <span class="alert_required bg-danger text-white p-1 rounded"
+                                        role="alert"><strong>{{ $message }}</strong></span>
+                                @enderror
                             </section>
                             <section class="col-12 my-3">
                                 <button class="btn btn-primary btn-sm">ثبت</button>
@@ -95,5 +108,22 @@ id="image">
     <script src="{{ asset('admin-assets/ckeditor/ckeditor.js') }}"></script>
     <script>
         CKEDITOR.replace('description');
+    </script>
+
+    <script>
+        $(document).ready(function() {
+            var tags_input = $('#tags');
+            var select_tags = $('#select_tags');
+            select_tags.select2({
+                placeholder: 'لطفا تگ های خود را وارد کنید.',
+                tags: true
+            });
+            $('#form').submit(function(event){
+                if (select_tags.val() !== null && select_tags.val().length > 0){
+                    var selectedSource = select_tags.val().join(',');
+                    tags_input.val(selectedSource);
+                }
+            })
+        })
     </script>
 @endsection
