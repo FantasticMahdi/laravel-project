444aafb32989f31245dfb9d90c8cf751db840875
FantasticMahdi <mahdi.khrasani95@gmail.com>
2023-09-29 16:46:16 +0330

    part-73 (select2 old value for edit&create)

--- resources/views/admin/content/category/create.blade.php
+++ resources/views/admin/content/category/create.blade.php
@@ -27,7 +27,8 @@
                 </section>
 
                 <section>
-                    <form action="{{ route('admin.content.category.store') }}" method="post" enctype="multipart/form-data" id="form">
+                    <form action="{{ route('admin.content.category.store') }}" method="post" enctype="multipart/form-data"
+                        id="form">
                         @csrf
                         <section class="row">
                             <section class="col-12 col-md-6 my-2">
@@ -44,10 +45,11 @@
                             </section>
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
-<label for="tags">تگ ها</label>
-<input class="form-control form-control-sm" type="hidden" name="tags" id="tags"
-value="{{ old('tags') }}">
-<select name="" class="select3 form-control form-control-sm" id="select_tags" multiple>
+                                    <label for="tags">تگ ها</label>
+                                    <input class="form-control form-control-sm" type="hidden" name="tags" id="tags"
+                                        value="{{ old('tags') }}">
+                                    <select name="" class="select3 form-control form-control-sm" id="select_tags"
+                                        multiple>
 
                                     </select>
                                 </div>
@@ -114,12 +116,24 @@ value="{{ old('tags') }}">
         $(document).ready(function() {
             var tags_input = $('#tags');
             var select_tags = $('#select_tags');
+            var default_tags = tags_input.val();
+            var default_data = null;
+
+            if(tags_input.val() !== null && tags_input.val().length > 0){
+                default_data = default_tags.split(',');
+            }
+
+
             select_tags.select2({
                 placeholder: 'لطفا تگ های خود را وارد کنید.',
-                tags: true
+                tags: true,
+                data : default_data
             });
-            $('#form').submit(function(event){
-                if (select_tags.val() !== null && select_tags.val().length > 0){
+            select_tags.children('option').attr('selected', true).trigger('change');
+
+
+            $('#form').submit(function(event) {
+                if (select_tags.val() !== null && select_tags.val().length > 0) {
                     var selectedSource = select_tags.val().join(',');
                     tags_input.val(selectedSource);
                 }
--- resources/views/admin/content/category/edit.blade.php
+++ resources/views/admin/content/category/edit.blade.php
@@ -27,7 +27,7 @@
                 </section>
 
                 <section>
-                    <form action="{{ route('admin.content.category.update', $postCategory->id) }}" method="post" enctype="multipart/form-data">
+                    <form action="{{ route('admin.content.category.update', $postCategory->id) }}" method="post" enctype="multipart/form-data" id="form">
                         @csrf
                         {{ method_field('put') }}
                         <section class="row">
@@ -43,15 +43,19 @@ value="{{ old('name', $postCategory->name) }}">
                                         role="alert"><strong>{{ $message }}</strong></span>
                                 @enderror
                             </section>
-                            <section class="col-12 col-md-6 my-2">
-                                <div class="form-group">
-                                    <label for="tags">تگ ها</label>
-                                    <input class="form-control form-control-sm" type="text" name="tags" id="tags"
-                                        value="{{ old('tags', $postCategory->tags) }}">
-                                </div>
-                                @error('tags')
-                                    <span class="alert_required bg-danger text-white p-1 rounded"
-                                        role="alert"><strong>{{ $message }}</strong></span>
+<section class="col-12 col-md-6 my-2">
+<div class="form-group">
+<label for="tags">تگ ها</label>
+<input class="form-control form-control-sm" type="hidden" name="tags" id="tags"
+value="{{ old('tags', $postCategory->tags) }}">
+<select name="" class="select3 form-control form-control-sm" id="select_tags" multiple>
+
+</select>
+</div>
+@error('tags')
+<span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+<strong>{{ $message }}</strong>
+</span>
                                 @enderror
                             </section>
                             <section class="col-12 col-md-6 my-2">
@@ -105,5 +109,34 @@ value="{{ old('name', $postCategory->name) }}">
     <script src="{{ asset('admin-assets/ckeditor/ckeditor.js') }}"></script>
     <script>
         CKEDITOR.replace('description');
+    </script>
+
+    <script>
+        $(document).ready(function() {
+            var tags_input = $('#tags');
+            var select_tags = $('#select_tags');
+            var default_tags = tags_input.val();
+            var default_data = null;
+
+            if(tags_input.val() !== null && tags_input.val().length > 0){
+                default_data = default_tags.split(',');
+            }
+
+
+            select_tags.select2({
+                placeholder: 'لطفا تگ های خود را وارد کنید.',
+                tags: true,
+                data : default_data
+            });
+            select_tags.children('option').attr('selected', true).trigger('change');
+
+
+            $('#form').submit(function(event) {
+                if (select_tags.val() !== null && select_tags.val().length > 0) {
+                    var selectedSource = select_tags.val().join(',');
+                    tags_input.val(selectedSource);
+                }
+            })
+        })
     </script>
 @endsection
