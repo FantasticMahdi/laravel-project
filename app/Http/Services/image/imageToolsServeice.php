dfc13bbf0944f6719d9e782516e47b017b3449c5
FantasticMahdi <mahdi.khrasani95@gmail.com>
2023-10-02 08:07:31 +0330

    part-83 (create Image upload service)

---
+++ app/Http/Services/image/imageToolsServeice.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Services\Image;
+
+class ImageToolsService
+{
+
+    protected $image;
+    protected $exclusiveDirectory;
+    protected $imageDirectory;
+    protected $imageName;
+    protected $imageFormat;
+    protected $finalImageDirectory;
+    protected $finalImageName;
+
+
+    
+}
