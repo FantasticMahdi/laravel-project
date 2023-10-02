deab4b77cc9ceee40bd5433ec5bfef81b3a9df74
FantasticMahdi <mahdi.khrasani95@gmail.com>
2023-10-02 16:15:45 +0330

    part-84 (create Image upload service)-2

--- app/Http/Services/image/imageToolsServeice.php
+++ app/Http/Services/image/imageToolsServeice.php
@@ -13,6 +13,73 @@ class ImageToolsService
     protected $finalImageDirectory;
     protected $finalImageName;
 
+    public function setImage($image)
+    {
+        $this->image = $image;
+    }
 
-    
+    public function getExclusiveDirectory()
+    {
+        return $this->exclusiveDirectory;
+    }
+
+    public function setExclusiveDirectory($exclusiveDirectory)
+    {
+        $this->exclusiveDirectory = trim($exclusiveDirectory, '/\\');
+    }
+
+    public function getImageDirectory()
+    {
+        return $this->imageDirectory;
+    }
+
+    public function setImageDirectory($imageDirectory)
+    {
+        $this->imageDirectory = trim($imageDirectory, '/\\');
+    }
+
+    public function getImageName()
+    {
+        return $this->imageName;
+    }
+
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+    }
+
+    public function setCurrentImageName()
+    {
+        return !empty($this->image) ? $this->setImageName(pathinfo($this->image->getClientOriginalName(), PATHINFO_FILENAME)) : false;
+    }
+
+    public function getImageFormat()
+    {
+        return $this->imageFormat;
+    }
+
+    public function setImageFormat($imageFormat)
+    {
+        $this->imageFormat = $imageFormat;
+    }
+
+    public function getFinalImageDirectory()
+    {
+        return $this->finalImageDirectory;
+    }
+
+    public function setFinalImageDirectory($finalImageDirectory)
+    {
+        $this->finalImageDirectory = $finalImageDirectory;
+    }
+
+    public function getFinalImageName()
+    {
+        return $this->finalImageName;
+    }
+
+    public function setFinalImageName($finalImageName)
+    {
+        $this->finalImageName = $finalImageName;
+    }
 }
