a559026fcd74520459ded0f155e0b995f996ad90
FantasticMahdi <mahdi.khrasani95@gmail.com>
2023-10-04 16:45:13 +0330

    part-90 (create Image cache service)-8

--- config/image.php
+++ config/image.php
@@ -37,4 +37,24 @@ return [
 
     'default-current-index-image' => 'medium',
 
+
+     //index size
+     'cache-image-sizes' => [
+        'large' => [
+            'width' => 800,
+            'height' => 600
+        ],
+        'medium' => [
+            'width' => 400,
+            'height' => 300
+        ],
+        'small' => [
+            'width' => 80,
+            'height' => 60
+        ],
+    ],
+
+    'default-current-cache-image' => 'medium',
+    'image-cache-life-time' => 10,
+    'image-not-found' => '',
 ];
