e19134599aed53f1ba6ceb7882f75662414dc1c9
FantasticMahdi <mahdi.khrasani95@gmail.com>
2023-10-05 20:09:34 +0330

    part-91 (complete Image service update&delete)-9

--- app/Http/Controllers/Admin/Content/CategoryController.php
+++ app/Http/Controllers/Admin/Content/CategoryController.php
@@ -86,10 +86,30 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(PostCategoryRequest $request, PostCategory $postCategory)
+    public function update(PostCategoryRequest $request, PostCategory $postCategory, ImageService $imageService)
     {
         $inputs = $request->all();
-        $inputs['image'] = 'image';
+
+        if ($request->hasFile('image')) {
+            if (!empty($postCategory->image)) {
+$imageService->deleteDirectoryAndFiles($postCategory->image['directory']);
+            }
+$imageService->setExclusiveDirectory('images' . DIRECTORY_SEPARATOR . 'post-category');
+
+            $result = $imageService->createIndexAndSave($request->file('image'));
+            if ($result === false) {
+                return redirect()->route('admin.content.category.index')->with('swal-error', 'آپلود تصویر با خطا مواجه شد.');
+            }
+            $inputs['image'] = $result;
+        } else {
+            if(isset($inputs['currentImage']) && !empty($postCategory->image)){
+                $image = $postCategory->image;
+                $image['currentImage'] = $inputs['currentImage'];
+                $inputs['image'] = $image;
+            }
+        }
+
+
         $postCategory->update($inputs);
         return redirect()->route('admin.content.category.index')->with('swal-success', 'دسته بندی با موفقیت ویرایش شد');
     }
--- resources/views/admin/content/category/edit.blade.php
+++ resources/views/admin/content/category/edit.blade.php
@@ -60,14 +60,35 @@ value="{{ old('tags', $postCategory->tags) }}">
                             </section>
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
-                                    <label for="image">تصویر</label>
-                                    <input class="form-control form-control-sm" type="file" name="image"
-                                        id="image">
+<label for="image">تصویر</label>
+<input class="form-control form-control-sm" type="file" name="image"
+id="image">
                                 </div>
                                 @error('image')
                                     <span class="alert_required bg-danger text-white p-1 rounded"
                                         role="alert"><strong>{{ $message }}</strong></span>
                                 @enderror
+
+<section class="row">
+    @php
+        $number = 1;
+        @endphp
+        @foreach ($postCategory->image['indexArray'] as $key => $value)
+
+<section class="col-md-{{ 6/ $number }}">
+    <div class="form-check">
+        <input type="radio" class="form-check-input" name="currentImage" id="{{ $number }}" value="{{ $key }}" @if ($postCategory->image['currentImage'] == $key) checked @endif>
+        <label for="{{ $number }}" class="form-check-label mx-2">
+            <img src="{{ asset($value) }}" class="w-100" alt="" srcset="">
+        </label>
+    </div>
+</section>
+@php
+    $number++;
+@endphp
+@endforeach
+                                </section>
+
                             </section>
                             <section class="col-12 col-md-6 my-2">
                                 <div class="form-group">
