89ac3b7e15fd503e16e5599077a5066e11a7fd83
FantasticMahdi <mahdi.khrasani95@gmail.com>
2023-11-14 17:46:19 +0330

    part-94 (use jalaliDate)

--- resources/views/admin/content/post/create.blade.php
+++ resources/views/admin/content/post/create.blade.php
@@ -3,6 +3,7 @@
 
 @section('head-tag')
     <title>ایجاد پست</title>
+    <link rel="stylesheet" href="{{ asset('admin-assets/jalalidatepicker/persian-datepicker.min.css') }}">
 @endsection
 
 @section('content')
@@ -27,46 +28,51 @@
                 </section>
 
                 <section>
-                    <form action="{{ route('admin.content.post.store') }}" method="POST" enctype="multipart/form-data" id="form">
+                    <form action="{{ route('admin.content.post.store') }}" method="POST" enctype="multipart/form-data"
+                        id="form">
                         @csrf
                         <section class="row">
                             <section class="col-12 col-md-6">
                                 <div class="form-group">
-<label for="">عنوان پست</label>
-<input class="form-control form-control-sm" type="text" name="title" id="" value="{{ old('title') }}">
+                                    <label for="">عنوان پست</label>
+                                    <input class="form-control form-control-sm" type="text" name="title" id=""
+                                        value="{{ old('title') }}">
                                 </div>
                                 @error('title')
-                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </section>
                             <section class="col-12 col-md-6">
                                 <div class="form-group">
-<label for="">انتخاب دسته</label>
-<select class="form-control form-control-sm" name="category_id" id="">
-<option value="">choose category</option>
-@foreach($postCategories as $postCategory)
-<option value="{{ $postCategory->id }}" @if (old('category_id') == $postCategory->id) selected @endif>{{ $postCategory->name }}</option>
-@endforeach
+                                    <label for="">انتخاب دسته</label>
+                                    <select class="form-control form-control-sm" name="category_id" id="">
+                                        <option value="">choose category</option>
+                                        @foreach ($postCategories as $postCategory)
+                                            <option value="{{ $postCategory->id }}"
+                                                @if (old('category_id') == $postCategory->id) selected @endif>{{ $postCategory->name }}
+                                            </option>
+                                        @endforeach
                                     </select>
                                 </div>
                                 @error('category_id')
-                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </section>
                             <section class="col-12 col-md-6">
                                 <div class="form-group">
-<label for="">تصویر</label>
-<input class="form-control form-control-sm" type="file" name="image" id="">
+                                    <label for="">تصویر</label>
+                                    <input class="form-control form-control-sm" type="file" name="image"
+                                        id="">
                                 </div>
                                 @error('iamge')
-                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </section>
                             <section class="col-12 col-md-6">
                                 <div class="form-group">
@@ -87,7 +93,7 @@
                             <section class="col-12 col-md-6">
                                 <div class="form-group">
                                     <label for="commentable">امکان درج کامنت</label>
-<select class="form-control form-control-sm" name="commentable" id="commentable">
+                                    <select class="form-control form-control-sm" name="commentable" id="commentable">
                                         <option value="0" @if (old('commentable') == 0) selected @endif>غیر فعال
                                         </option>
                                         <option value="1" @if (old('commentable') == 1) selected @endif>فعال
@@ -102,19 +108,21 @@
                             <section class="col-12 col-md-6">
                                 <div class="form-group">
                                     <label for="">تاریخ انتشار</label>
-                                    <input class="form-control form-control-sm" type="text" name="published_at"
-                                        id="">
+                                    <input class="form-control form-control-sm d-none" type="text" name="published_at"
+                                        id="published_at">
+                                    <input class="form-control form-control-sm" type="text" id="published_at_view">
                                 </div>
                                 @error('published_at')
-                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </section>
                             <section class="col-12">
                                 <div class="form-group">
                                     <label for="tags">تگ ها</label>
-<input class="form-control form-control-sm" type="hidden" name="tags" id="tags" value="{{ old('tags') }}">
+                                    <input class="form-control form-control-sm" type="hidden" name="tags" id="tags"
+                                        value="{{ old('tags') }}">
                                     <select name="" class="select3 form-control form-control-sm" id="select_tags"
                                         multiple>
 
@@ -129,13 +137,13 @@
                             <section class="col-12">
                                 <div class="form-group">
                                     <label for="">خلاصه پست</label>
-<textarea class="form-control form-control-sm" rows="4" name="summary" id="summary">{{ old('summary') }}</textarea>
+                                    <textarea class="form-control form-control-sm" rows="4" name="summary" id="summary">{{ old('summary') }}</textarea>
                                 </div>
                                 @error('summary')
-                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </section>
                             <section class="col-12">
                                 <div class="form-group">
@@ -143,10 +151,10 @@
                                     <textarea class="form-control form-control-sm" rows="4" name="body" id="body">{{ old('body') }}</textarea>
                                 </div>
                                 @error('body')
-                                <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
-                                    <strong>{{ $message }}</strong>
-                                </span>
-                            @enderror
+                                    <span class="alert_required bg-danger text-white p-1 rounded" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
                             </section>
                             <section class="col-12">
                                 <button class="btn btn-primary btn-sm">ثبت</button>
@@ -161,6 +169,8 @@
 
 @section('script')
     <script src="{{ asset('admin-assets/ckeditor/ckeditor.js') }}"></script>
+    <script src="{{ asset('admin-assets/jalalidatepicker/persian-date.min.js') }}"></script>
+    <script src="{{ asset('admin-assets/jalalidatepicker/persian-datepicker.min.js') }}"></script>
     <script>
         CKEDITOR.replace('body');
         CKEDITOR.replace('summary');
@@ -168,30 +178,39 @@
 
     <script>
         $(document).ready(function() {
-            var tags_input = $('#tags');
-            var select_tags = $('#select_tags');
-            var default_tags = tags_input.val();
-            var default_data = null;
-
-            if (tags_input.val() !== null && tags_input.val().length > 0) {
-                default_data = default_tags.split(',');
-            }
-
-
-            select_tags.select2({
-                placeholder: 'لطفا تگ های خود را وارد کنید.',
-                tags: true,
-                data: default_data
-            });
-            select_tags.children('option').attr('selected', true).trigger('change');
-
-
-            $('#form').submit(function(event) {
-                if (select_tags.val() !== null && select_tags.val().length > 0) {
-                    var selectedSource = select_tags.val().join(',');
-                    tags_input.val(selectedSource);
-                }
+            $('#published_at_view').persianDatepicker({
+                format: 'YYYY/MM/DD',
+                altField: '#published_at'
             })
-        })
+        });
     </script>
-@endsection
+
+    <script>
+        $(document).ready(function() {
+                var tags_input = $('#tags');
+                var select_tags = $('#select_tags');
+                var default_tags = tags_input.val();
+                var default_data = null;
+
+                if (tags_input.val() !== null && tags_input.val().length > 0) {
+                    default_data = default_tags.split(',');
+                }
+
+
+                select_tags.select2({
+                    placeholder: 'لطفا تگ های خود را وارد کنید.',
+                    tags: true,
+                    data: default_data
+                });
+                select_tags.children('option').attr('selected', true).trigger('change');
+
+
+                $('#form').submit(function(event) {
+                    if (select_tags.val() !== null && select_tags.val().length > 0) {
+                        var selectedSource = select_tags.val().join(',');
+                        tags_input.val(selectedSource);
+                    }
+                })
+            }) <
+            /scriptscript>
+    @endsection
