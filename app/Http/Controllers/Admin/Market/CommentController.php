7c79e7cc82d830a316d70fbb9a6465212589fcd9
FantasticMahdi <mahdi.khrasani95@gmail.com>
2025-03-19 19:25:31 +0330

    part-136(market comments)

--- app/Http/Controllers/Admin/Market/CommentController.php
+++ app/Http/Controllers/Admin/Market/CommentController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin\Market;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\Content\CommentRequest;
+use App\Models\Content\Comment;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
@@ -14,7 +16,13 @@ class CommentController extends Controller
      */
     public function index()
     {
-        return view('admin.market.comment.index');
+        $unSeenComments = Comment::where([['commentable_type', 'App\Models\Market\Product'], ['seen', 0]])->get();
+        foreach ($unSeenComments as $unSeenComment) {
+            $unSeenComment->seen = 1;
+            $result = $unSeenComment->save();
+        }
+        $comments = Comment::orderBy('created_at', 'desc')->where('commentable_type', 'App\Models\Market\Product')->simplePaginate(15);
+        return view('admin.market.comment.index', ['comments' => $comments]);
     }
 
     /**
@@ -30,7 +38,7 @@ class CommentController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -41,7 +49,7 @@ class CommentController extends Controller
     /**
      * Display the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function show()
@@ -52,7 +60,7 @@ class CommentController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -63,8 +71,8 @@ class CommentController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param \Illuminate\Http\Request $request
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -75,11 +83,59 @@ class CommentController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  int  $id
+     * @param int $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
     {
         //
+    }
+
+    public function status(Comment $comment)
+    {
+        $comment->status = !$comment->status;
+        $result = $comment->save();
+        if ($result) {
+            if ($comment->status == 0) {
+                return response()->json(['status' => true, 'checked' => false]);
+            }
+            else{
+                return response()->json(['status' => true, 'checked' => true]);
+            }
+        }
+        else{
+            return response()->json(['status' => false]);
+        }
+    }
+
+    public function approved(Comment $comment)
+    {
+        $comment->approved = $comment->approved == 0 ? 1 : 0;
+        $result = $comment->save();
+        if ($result) {
+            return redirect()->route('admin.market.comment.index')->with('swal-success', ' وضعیت تایید کامنت با موفقیت تغییر کرد');
+        } else {
+            return redirect()->route('admin.market.comment.index')->with('swal-error', 'وضعیت کامنت با خطا مواجه شد');
+        }
+    }
+
+    public function answer(CommentRequest $request, Comment $comment)
+    {
+
+        if ($comment->parent == null) {
+
+            $inputs = $request->all();
+            $inputs['author_id'] = 1;
+            $inputs['parent_id'] = $comment->id;
+            $inputs['commentable_id'] = $comment->commentable_id;
+            $inputs['commentable_type'] = $comment->commentable_type;
+            $inputs['approved'] = 1;
+            $inputs['status'] = 1;
+
+            $comment = Comment::create($inputs);
+            return redirect()->route('admin.market.comment.index')->with('swal-success', 'کامنت با موفقیت ثبت شد');
+        } else {
+            return redirect()->route('admin.market.comment.index')->with('swal-error', 'برای این کامنت پاسخی ثبت شده است');
+        }
     }
 }
