044239caf7164904b27a76fa02c4fc00081bd80c
FantasticMahdi <mahdi.khrasani95@gmail.com>
2025-06-03 19:33:05 +0330

    part-178(convert numbers to persian)

--- resources/views/customer/market/product/product.blade.php
+++ resources/views/customer/market/product/product.blade.php
@@ -496,17 +496,23 @@
                 number = parseFloat($('#number').val());
             }
 
-            if ($('#product_discount_price').length != 0) {
-                product_discount_price = parseFloat($('#product-discount-price'))
+            if ($('#product-discount-price').length != 0) {
+                product_discount_price = parseFloat($('#product-discount-price').attr('data-product-discount-price'))
             }
-
 
             var product_price = product_original_price + selected_color_price + selected_guarantee_price;
             var final_price = number * (product_price - product_discount_price);
 
+            $('#product_price').html(toFarsiNumber(product_price));
+            $('#final_price').html(toFarsiNumber(final_price));
+        }
 
-            $('#product_price').html(product_price);
-            $('#final_price').html(final_price);
+        function toFarsiNumber(number) {
+            const farsiDigits = ['۰', '۱', '۲', '۳', '۴', '۵', '۶', '۷', '۸', '۹'];
+            //add comma
+            number = new Intl.NumberFormat().format(number);
+            //convert to persian
+            return number.toString().replace(/\d/g, x => farsiDigits[x]);
         }
     </script>
 @endsection
