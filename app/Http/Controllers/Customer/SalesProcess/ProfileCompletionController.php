28768a69c6b06d1618185f7a089091ca8433801f
FantasticMahdi <mahdi.khrasani95@gmail.com>
2025-06-28 19:20:07 +0330

    part-194(competing profile)

--- app/Http/Controllers/Customer/SalesProcess/ProfileCompletionController.php
+++ app/Http/Controllers/Customer/SalesProcess/ProfileCompletionController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Customer\SalesProcess;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Customer\SalesProcess\ProfileCompletionRequest;
 use App\Models\Market\CartItem;
-use Illuminate\Http\Request;
 
 class ProfileCompletionController extends Controller
 {
@@ -16,17 +16,39 @@ class ProfileCompletionController extends Controller
         return view('customer.sales-process.profile-completion', ['cartItems' => $cartItems, 'user' => $user]);
     }
 
-    public function profileUpdate(Request $request)
+    public function profileUpdate(ProfileCompletionRequest $request)
     {
-        $request->validate([
-            'first_name' => 'sometimes|required',
-            'last_name' => 'sometimes|required',
-            'email' => 'email|unique:users,email',
-            'mobile' => 'sometimes|required|unique:users,mobile',
-            'national_code' => 'sometimes|required|unique:users,national_code',
-        ]);
-        $inputs = $request->all();
-        auth()->user()->update($inputs);
+        $user = auth()->user();
+        $inputs = [];
+
+        // ✅ mobile
+        if (isset($request->mobile) && empty($user->mobile)) {
+            $mobile = convertPersianToEnglish($request->mobile);
+            if (preg_match('/^(\+98|98|0)9\d{9}$/', $mobile)) {
+                $mobile = ltrim($mobile, '0');
+                $mobile = str_starts_with($mobile, '98') ? substr($mobile, 2) : $mobile;
+                $mobile = str_replace('+98', '', $mobile);
+                $inputs['mobile'] = $mobile;
+            } else {
+                return redirect()->back()->withErrors(['mobile' => 'فرمت شماره موبایل معتبر نمی‌باشد!']);
+            }
+        }
+
+        // ✅ email
+        if (isset($request->email) && empty($user->email)) {
+            $inputs['email'] = convertPersianToEnglish($request->email);
+        }
+
+        // ✅ سایر فیلدها (با array_filter)
+        $inputs += array_filter($request->only([
+            'first_name',
+            'last_name',
+            'national_code',
+        ]));
+        if (!empty($inputs)) {
+            $user->update($inputs);
+        }
+
         return redirect()->route('customer.sales-process.address-and-delivery');
     }
 }
